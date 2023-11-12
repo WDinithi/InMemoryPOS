51c8ed5faca796fe85a810ad838009bbcd686830
WDinithi <wijewardanadinithi@gmail.com>
2023-11-12 11:56:32 +0530

    commit

---
+++ db/db.js
@@ -0,0 +1,14 @@
+//you can store data like as follows
+var customerDB = [
+    {id: "C00-001", name: "Malsha Amandi", address: "Matara", salary: 100000},
+    {id: "C00-002", name: "Senali Jayathma", address: "Weligama", salary: 200000},
+    {id: "C00-003", name: "Randi Lakma", address: "Galle", salary: 300000}
+];
+
+var itemDB = [
+    {code:"I00-001",name:"Skirt",price: 145.00,qty: 100},
+    {code:"I00-002",name:"Blouse",price: 345.00,qty: 150},
+    {code:"I00-003",name:"Trouser",price: 245.00,qty: 400}
+];
+
+var orderDB= [];
